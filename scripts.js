572f013251e5a1feb1094bf67748b357cff39997
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-04-06 16:14:57 -0300

    entrada

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-function minhaFuncao() {
+function entrada() {
     let quantidade = prompt("Quantas cartas deseja")
 
-    if (quantidade < 2 & quantidade > 14) {
-        alert("dado inválido")
-        quantidade = prompt("Quantas cartas deseja")
+    while (quantidade > 14 || quantidade == 0 || quantidade < 4 || quantidade == null || quantidade%2 != 0) {
+       
+        quantidade = prompt("Comando inválido.  Quantas cartas deseja")
+       
     }
 }
