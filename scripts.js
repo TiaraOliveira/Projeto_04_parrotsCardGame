a9657a6bc5c17145188d55f770b919302768c4d4
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-04-08 14:31:35 -0300

    girando errado

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function cartasnamesa() {
         let cards = document.querySelector(".face");
 
         cards.innerHTML += `
-        <li>
+        <li class="cadafrente">
             <div>
                 <img src="/imagens/front.png" alt="">
              </div>
@@ -28,15 +28,15 @@ function cartasnamesa() {
         contador++;
 
     }
-    
+
 }
 
 function versocartas() {
     i = 0;
     const versos = [
-        `<img src="/imagens/explodyparrot.gif" alt=""></img>`,
-        `<img src="/imagens/explodyparrot.gif" alt=""></img>`,
-        `<img src="/imagens/fiestaparrot.gif" alt="">`,  
+        `<img src="/imagens/explodyparrot.gif" alt="">`,
+        `<img src="/imagens/explodyparrot.gif" alt="">`,
+        `<img src="/imagens/fiestaparrot.gif" alt="">`,
         `<img src="/imagens/fiestaparrot.gif" alt=""> `,
         `<img src="/imagens/revertitparrot.gif" alt="">`,
         `<img src="/imagens/revertitparrot.gif" alt="">`,
@@ -49,16 +49,26 @@ function versocartas() {
         `<img src="/imagens/bobrossparrot.gif" alt="">`,
         `<img src="/imagens/bobrossparrot.gif" alt="">`,
     ]
-    
-    versos.sort(comparador); 
+
+
+    let v1 = 0;
+    const versocompleto = [];
+    while (v1 < quantidade) {
+        versocompleto.push(versos[v1])
+
+        v1++;
+
+    }
+
+    versocompleto.sort(comparador);
     while (i < quantidade) {
         let back = document.querySelector(".verset");
 
         back.innerHTML += `
-         <li>
+         <li class="cadafrente">
             <div>
                 <span>
-                     ${versos[i]}      
+                     ${versocompleto[i]} 
                 </span>
             </div>
          </li>
@@ -66,14 +76,14 @@ function versocartas() {
         i++;
 
     }
-    
-    
+
+
 }
 
 
 
 
 
-function comparador() { 
-	return Math.random() - 0.5; 
+function comparador() {
+    return Math.random() - 0.5;
 }
